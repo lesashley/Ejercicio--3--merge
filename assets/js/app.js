ee45c2cb1a57b925283b4a6ece306cc03aea43c4
lesashley <lesashley56@gmail.com>
2017-03-05 22:01:23 -0500

    Integrando funciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,50 @@
-document.getElementById("calcular").onclick = function Peso() {
+function calculoPeso(){
+var gMercurio = 3.72;
+var gVenus = 8.82;
+var gMarte = 3.72;
+var gTierra = 9.8;
+var gJupiter = 23.13;
+var gSaturno = 9.01;
+var gUrano = 8.72;
+var gNeptuno = 10-97;
+var gPluton = 0.65;
+
+var peso = document.getElementById('peso').value;
+
+var planeta = document.getElementById('planeta').value;
+var resultado ;
+
+	if(planeta == "Mercurio"){
+		resultado = peso / gTierra * gMercurio;
+	}
+	if(planeta == "Venus"){
+		resultado = peso / gTierra * gVenus;
+	}
+	if(planeta == "Marte"){
+		resultado = peso / gTierra * gMarte;
+	}
+	if(planeta == "Tierra"){
+		resultado = peso / gTierra * gTierra;
+	}
+	if(planeta == "Jupiter"){
+		resultado = peso / gTierra * gJupiter;
+	}
+	if(planeta == "Saturno"){
+		resultado = peso / gTierra * gSaturno;
+	}
+	if(planeta == "Urano"){
+		resultado = peso / gTierra * gUrano;
+	}
+	if(planeta == "Neptuno"){
+		resultado = peso / gTierra * gNeptuno;
+	}
+	if(planeta == "Venus"){
+		resultado = peso / gTierra * gVenus;
+	}
+
+	document.getElementById('resultado').innerHTML = "Tu Peso en " + planeta + " es: "+(resultado).toFixed(2);
+}
+var calcula = document.getElementById('todo') ;
+calcula.onclick = function(){
+
 }
