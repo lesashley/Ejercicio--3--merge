7c56fcce277b4cf17b19d5d6ce8457e5f3e19d0f
miriampc <miiriampc@gmail.com>
2017-03-05 21:10:41 -0500

    añadiendo calculo de peso en funcion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,8 @@
 document.getElementById("calcular").onclick = function Peso() {
+  var name=document.getElementById('name').value;
+  var peso=document.getElementById('peso').value;
+  peso=(peso/9.78);
+
+  alert("Hola "+name+" tu peso en estos planetas es:\n"
+        +"Marte: "+3.71*peso+"\nMercurio: "+3.72*peso+"\nVenus: "+8.82*peso);
 }
